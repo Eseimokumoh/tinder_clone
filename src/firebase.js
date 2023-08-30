03a25e1274773aa329f0a2c1bc8a66ca0b694547
Eseimokumoh <119541134+Eseimokumoh@users.noreply.github.com>
2023-08-31 05:53:28 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCHNsNzTu_BXn6qqvQ5eadg7ZRmQHdeC3g",
+    apiKey: "",
     authDomain: "tinder-clone-a4ee8.firebaseapp.com",
     projectId: "tinder-clone-a4ee8",
     storageBucket: "tinder-clone-a4ee8.appspot.com",
     messagingSenderId: "788352286724",
-    appId: "1:788352286724:web:b09106ec62c3632e33b551",
+    appId: "",
     measurementId: "G-PQFTZWMQ2F",
   };
 
